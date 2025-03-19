1efd59033374fe92f30cfba624c1612baa110b15
Sol Alcaraz <alcarazmsol@gmail.com>
2025-03-19 13:38:02 -0300

    Add status badges de Build

---
+++ README.mk
@@ -0,0 +1,3 @@
+# Nuevo Proyecto
+
+[![build](https://github.com/solalcaraz/proyectoPruebaCI/actions/workflows/build.yml/badge.svg)](https://github.com/solalcaraz/proyectoPruebaCI/actions/workflows/build.yml)
